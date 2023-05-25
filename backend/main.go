94fb0aa19248598057b5f2b055d1860a9444061d
goverclock <goverclock@outlook.com>
2023-05-25 15:22:28 +0800

    remove admin login

--- backend/main.go
+++ backend/main.go
@@ -16,10 +16,25 @@ func main() {
 	}))
 
 	server.POST("/login/user", login_user)
-	server.POST("/login/admin", login_admin)
 	server.POST("/register/user", register_user)
 	server.POST("/charge/submit", authMiddleware, charge_submit)
 	server.GET("/charge/getChargingMsg", authMiddleware, charge_getChargingMsg)
-
+	// server.POST("/charge/chargeSubmit")	// maybe changeSubmit
+	// server.POST("/charge/cancelCharge")
+	// server.POST("/charge/startCharge")
+	// server.POST("/charge/endCharge")
+	// server.POST("/charge/details")
+	// server.POST("/recharge")
+	// server.POST("//getbalance")
+	// server.POST("/chargeports/getreport")
+	// server.POST("/chargeports/getchargeports")
+	// server.POST("/chargeports/addchargeport")
+	// server.POST("/chargeports/delBatch")
+	// server.POST("/chargeports/turnon")
+	// server.POST("/chargeports/setfailure")
+	// server.POST("/chargeports/waitingCars")
+	// server.POST("/system/getsettings")
+	// server.POST("/system/setsettings")
+	
 	server.Run(":8080")
 }
