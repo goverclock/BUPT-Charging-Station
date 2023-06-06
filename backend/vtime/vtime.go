c7aefb6ab1671ef7a6e25e7922dc73959c5e56b8
goverclock <goverclock@outlook.com>
2023-06-06 17:05:46 +0800

    vtime implemented

---
+++ backend/vtime/vtime.go
@@ -0,0 +1,44 @@
+package vtime
+
+import (
+	"sync"
+	"time"
+)
+
+const Xrate = 20
+
+type vtime struct {
+	ts int64
+	mu sync.Mutex
+}
+
+var vt vtime
+
+func init() {
+	now := time.Now().Unix()
+	hour := time.Now().Hour()
+	minu := time.Now().Minute()
+	seco := time.Now().Second()
+	now -= int64((hour*60+minu)*60 + seco)
+	now += 6 * 60 * 60 // begining hour = 6
+	vt.ts = now
+
+	go ticker()
+}
+
+func Now() time.Time {
+	vt.mu.Lock()
+	defer vt.mu.Unlock()
+	return time.Unix(vt.ts, 0)
+}
+
+func ticker() {
+	for {
+		time.Sleep(time.Second)
+		vt.mu.Lock()
+
+		vt.ts += Xrate
+
+		vt.mu.Unlock()
+	}
+}
